1435e49fc84f1c7cfc104fced3b97d661e0d594c
angelsNS <anavaro.sala@gmail.com>
2022-05-29 10:33:40 +0200

    [WIP] Climb mechanic

---
+++ UnrealProject/Intermediate/Build/Win64/UE4Editor/Inc/UnrealProject/UnrealProjectCharacter.generated.h
@@ -0,0 +1,97 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UNREALPROJECT_UnrealProjectCharacter_generated_h
+#error "UnrealProjectCharacter.generated.h already included, missing '#pragma once' in UnrealProjectCharacter.h"
+#endif
+#define UNREALPROJECT_UnrealProjectCharacter_generated_h
+
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_SPARSE_DATA
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_RPC_WRAPPERS
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAUnrealProjectCharacter(); \
+	friend struct Z_Construct_UClass_AUnrealProjectCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AUnrealProjectCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/UnrealProject"), NO_API) \
+	DECLARE_SERIALIZER(AUnrealProjectCharacter)
+
+
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAUnrealProjectCharacter(); \
+	friend struct Z_Construct_UClass_AUnrealProjectCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AUnrealProjectCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/UnrealProject"), NO_API) \
+	DECLARE_SERIALIZER(AUnrealProjectCharacter)
+
+
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AUnrealProjectCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AUnrealProjectCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AUnrealProjectCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUnrealProjectCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AUnrealProjectCharacter(AUnrealProjectCharacter&&); \
+	NO_API AUnrealProjectCharacter(const AUnrealProjectCharacter&); \
+public:
+
+
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AUnrealProjectCharacter(AUnrealProjectCharacter&&); \
+	NO_API AUnrealProjectCharacter(const AUnrealProjectCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AUnrealProjectCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUnrealProjectCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AUnrealProjectCharacter)
+
+
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__CameraBoom() { return STRUCT_OFFSET(AUnrealProjectCharacter, CameraBoom); } \
+	FORCEINLINE static uint32 __PPO__FollowCamera() { return STRUCT_OFFSET(AUnrealProjectCharacter, FollowCamera); }
+
+
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_9_PROLOG
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_SPARSE_DATA \
+	UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_RPC_WRAPPERS \
+	UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_INCLASS \
+	UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_SPARSE_DATA \
+	UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UNREALPROJECT_API UClass* StaticClass<class AUnrealProjectCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID UnrealProject_Source_UnrealProject_UnrealProjectCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30136
+#include "D:/TfGRepository/UnrealProject/Intermediate/Build/Win64/UnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
